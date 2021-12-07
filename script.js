52147cbc5eb931f56b8ef52c8d6ff775ee8df0ca
Mattias Kind <mattias.kind@gmail.com>
2021-12-07 15:17:31 +0100

    Skriptet hämtar data från api:t

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const form = document.querySelector('form');
 const input = document.querySelector('#input');
 
-const responseDiv = document.querySelector('#response');
+const wrapper = document.querySelector('.wrapper');
+const title = document.querySelector('.title-big');
+const director = document.querySelector('.director');
+const producer = document.querySelector('.producer');
+const releaseDate = document.querySelector('.release-date');
+const runningTime = document.querySelector('.running-time');
+const description = document.querySelector('.description');
+const image = document.querySelector('.image');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+
   const req = new XMLHttpRequest();
   req.open('GET', `https://ghibliapi.herokuapp.com/films?title=${input.value}`);
   req.onload = function () {
     const res = JSON.parse(this.response);
+    console.log(res);
     if (res.length === 0) return console.log('TOMT FÄLT');
-    responseDiv.innerHTML = `
-    <div>Title: ${res[0].title}</div>
-    <div>Producent: ${res[0].producer}</div>
-    <div>Beskrivning: ${res[0].description}</div>
-    <div><img src="${res[0].image}"></div>
-    `;
+
+    title.innerText = res[0].title;
+    director.innerText = res[0].director;
+    producer.innerText = res[0].producer;
+    releaseDate.innerText = res[0].release_date;
+    runningTime.innerText = res[0].running_time;
+    description.innerText = res[0].description;
+    image.innerHTML = `<img src="${res[0].image}">`;
+    if (wrapper.classList.contains('hidden')) wrapper.classList.remove('hidden');
   };
 
   req.send();
